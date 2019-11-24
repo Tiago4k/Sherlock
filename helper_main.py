4a6a08c8ad89cb5a4c122038900b081bb11997cd
Tiago <tiago4k@gmail.com>
2019-11-24 23:32:03 +0000

    Removed redundant imports & added train/val split

--- helper_main.py
+++ helper_main.py
@@ -1,35 +1,46 @@
 import os
-import numpy as np
 import shutil
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+
 import constant as const
 import Helper_Scripts.directory_helper as dh
 from Helper_Scripts.image_loader import load_img_name_array
-from tqdm import tqdm
-from sklearn.model_selection import train_test_split
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-
 
 # Display total images for each category
 print('Number of Tampered = {}'.format(len(os.listdir(const.PATH_TO_FAKES))))
 print('Number of Originals = {}'.format(len(os.listdir(const.PATH_TO_REALS))))
 
 # Load image names into X so we can Train/Test Split.
-# Load Y with an array of 2000 values, 1300 real, 700 fake.
+# Load Y with an array of 12614 values, 7491 real, 5123 fake.
 X = load_img_name_array(const.PATH_TO_REALS, const.PATH_TO_FAKES)
-Y = [0]*1300+[1]*700
+Y = [0]*7491+[1]*5123
 
-# Stratifying so we have approximately the same percentage of samples for in the train/test set.
-x_train, x_test, y_train, y_test = train_test_split(X,
-                                                    Y,
-                                                    test_size=0.33,
+# Stratifying so we have approximately the same percentage of samples for in the train/test/val set.
+
+#  Train/Val Split
+x_train, x_valid, y_train, y_valid = train_test_split(X,
+                                                      Y,
+                                                      test_size=0.33,
+                                                      stratify=Y)
+
+#  Train/Test Split
+x_train, x_test, y_train, y_test = train_test_split(x_train,
+                                                    y_train,
+                                                    test_size=0.1,
                                                     stratify=Y)
 
 # Create all needed directories
 print(dh.make_train_test_dir())
 
 # Move splitted images into the correct folders
-print(dh.move_to_train_folder(x_train, y_train, const.PATH_TO_REALS,
-                              const.PATH_TO_FAKES, const.PATH_TO_TRAIN_REALS, const.PATH_TO_TRAIN_FAKES))
+print(dh.move_to_folder(x_train, y_train, const.PATH_TO_REALS,
+                        const.PATH_TO_FAKES, const.PATH_TO_TRAIN_REALS, const.PATH_TO_TRAIN_FAKES))
 
-print(dh.move_to_test_folder(x_test, y_test, const.PATH_TO_REALS,
-                             const.PATH_TO_FAKES, const.PATH_TO_TEST_REALS, const.PATH_TO_TEST_FAKES))
+print(dh.move_to_folder(x_valid, y_valid, const.PATH_TO_REALS,
+                        const.PATH_TO_FAKES, const.PATH_TO_VALID_REALS, const.PATH_TO_VALID_FAKES))
+
+print(dh.move_to_folder(x_test, y_test, const.PATH_TO_TRAIN_REALS,
+                        const.PATH_TO_TRAIN_FAKES, const.PATH_TO_TEST_REALS, const.PATH_TO_TEST_FAKES))
