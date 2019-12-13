c93ef03221dbb9258edc9d9f54daa82bb687fc99
Tiago <tiago4k@gmail.com>
2019-12-13 00:46:17 +0000

    Predictions now working correctly

--- Backend/ServerSide/predict.py
+++ Backend/ServerSide/predict.py
@@ -1,12 +1,20 @@
 import os
 
-from fastai.vision import load_learner, open_image
+from fastai.vision import Path, load_learner, open_image
+
+from image_converter import ImageHandler
 
 
-def get_prediction(weights, path):
+def get_prediction(path):
+    """Returns confidence level and prediction of a given image.
+    Params:
+    path: path of the image to be predicted
+    """
 
-    learn = load_learner(weights)
+    cwd = os.getcwd()
+    weights_path = Path(cwd + '/Backend/ServerSide/')
 
+    learn = load_learner(weights_path)
     img = open_image(path)
 
     pred_class, pred_idx, confidence = learn.predict(img)
@@ -21,4 +29,20 @@ def get_prediction(weights, path):
     if conf >= 65:
         return (pred_class, conf)
 
-    return 'Unable to confidently provide a prediction for this image.'
+    return 0
+
+
+if __name__ == '__main__':
+
+    # img_path = '/Users/tiagoramalho/Downloads/Demo_Images/mercedes.jpg'
+    # img_obj = ImageHandler(path=img_path)
+
+    # ela_img = img_obj.convert_to_ela()
+    # result = get_prediction(ela_img)
+
+    # print('*' * 100)
+    # print('Prediction: {}'.format(result[0]))
+    # print('Confidence: {:.3f}%'.format(result[1]))
+    # print('*' * 100)
+
+    pass
