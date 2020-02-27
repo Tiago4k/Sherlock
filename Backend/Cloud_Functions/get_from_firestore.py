db0bc46f6dbc6ffa254bb564fd14eb12d5048280
Tiago <tiago4k@gmail.com>
2020-02-27 21:05:18 +0000

    Fixed the structuring of data

--- Backend/Cloud_Functions/get_from_firestore.py
+++ Backend/Cloud_Functions/get_from_firestore.py
@@ -2,7 +2,6 @@ from google.cloud import firestore
 from flask import jsonify
 
 db = firestore.Client()
-uuids = {}
 
 
 def main(request):
@@ -12,11 +11,30 @@ def main(request):
     user_ref = db.collection(u'Users').document(data['username'])
     docs = user_ref.collection(u'Uploads').stream()
 
-    uuids = {doc.id: doc.to_dict() for doc in docs}
+    # Simple list comprehension to return all documents found under a single user.
+    user_docs = {doc.id: doc.to_dict() for doc in docs}
+
+    # user_docs holds a dictonary of documents returned from firestore. Must convert it
+    # into a list so we can iterate through the uuids.
+    uuid_list = list(user_docs)
+
+    # Iterate through each of the uuid's and append it's information into a new dictonary.
+    # Dictonary returned is structured in which it is hard to pull out the correct information.
+    results_list = []
+
+    for i in range(len(uuid_list)):
+        results_dict = {
+            'uuid': uuid_list[i],
+            'prediction': user_docs[uuid_list[i]]['prediction'],
+            'confidence': user_docs[uuid_list[i]]['confidence'],
+            'bucket_name': user_docs[uuid_list[i]]['bucket name']
+        }
+        # Append the newly structured data to a list to send back to the client.
+        results_list.append(results_dict)
 
     resp = {
         'status': 200,
         'message': 'Data Successfully Retrieved.',
-        'documents': uuids}
+        'documents': results_list}
 
     return jsonify(resp)
